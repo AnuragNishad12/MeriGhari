7aba99a368e2c245dbd1051d20273d707f249445
SanjeevHo <21f2001185@ds.study.iitm.ac.in>
2025-03-07 18:30:54 +0530

    Plan Activity

---
+++ app/src/main/java/com/example/merighari/Pages/PlanActivity.kt
@@ -0,0 +1,12 @@
+package com.example.merighari.Pages
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.merighari.R
+
+class PlanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plan)
+    }
+}
